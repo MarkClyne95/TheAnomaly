a8268075e518ed3e0af07161e56e8fa717930880
MarkClyne95 <99036942+MarkClyne95@users.noreply.github.com>
2022-11-30 01:12:44 +0000

    Revert "BJS Editor update"
    This reverts commit 33ec8aca47de156a753908b1402a4eabd894ca07.

--- BJS Editor/declaration/src/assets/Scripts/DoorInteraction.d.ts
+++
@@ -1,8 +0,0 @@
-import { Mesh } from "@babylonjs/core";
-import "./player.ts";
-export declare class DoorInteraction extends Mesh {
-    onInitialize(): void;
-    constructor(player: player);
-    private CollisionDetection;
-    private _beforeRenderUpdate;
-}
--- BJS Editor/declaration/src/scenes/DoorInteraction.d.ts
+++
@@ -1,8 +0,0 @@
-import player from "./player";
-export default class DoorInteraction {
-    constructor();
-    playerRef: player;
-    onStart(): void;
-    onUpdate(): void;
-    onCollide(): void;
-}
