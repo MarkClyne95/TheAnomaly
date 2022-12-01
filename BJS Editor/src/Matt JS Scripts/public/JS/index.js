74ae2266a216705fcf50c3dc50bdb359023d34cb
DocHaunt <b00367881@studentmail.uws.ac.uk>
2022-12-01 17:03:44 +0000

    Matt JS files added for title

---
+++ BJS Editor/src/Matt JS Scripts/public/JS/index.js
@@ -0,0 +1,15 @@
+//import guiScene  from "./guiScene.js";
+import {scenes, scene, engine, setSceneIndex}  from "./createScenes.js";
+
+setSceneIndex(0);
+
+//let gui = guiScene(engine);
+//gui.autoClear = false;
+engine.runRenderLoop(() => {
+    scene.render();
+    //gui.render();
+});
+
+/*******************************************************
+*  YOU SHOULDN'T NEED TO ADD ANY ADDITIONAL CODE HERE  *
+********************************************************/
