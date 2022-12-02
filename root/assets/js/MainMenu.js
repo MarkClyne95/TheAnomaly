256890d2a7a1bc1512c9d10f0357141c336752f8
MarkClyne95 <99036942+MarkClyne95@users.noreply.github.com>
2022-12-02 16:21:04 +0000

    changes

--- root/assets/js/MainMenu.js
+++ root/assets/js/MainMenu.js
@@ -1,138 +1,138 @@
 //import { title } from "process";
-import {setSceneIndex}        from "./createScenes.js";
+import { setSceneIndex } from "./createScenes.js";
 
 function createCamera(scene) {
-  let camAlpha = -Math.PI / 2;
-  let camBeta = Math.PI / 2.5;
-  let camDist = 15;
-  let camTarget = new BABYLON.Vector3(0, 0, 0);
-  let camera = new BABYLON.ArcRotateCamera(
-    "camera1",
-    camAlpha,
-    camBeta,
-    camDist,
-    camTarget,
-    scene
-  );
-  //camera.attachControl(true); // YOU CANNOT ATTACHCONTROL TO CAMERA WITH MULTIPLE SCENES
-  return camera;
+    let camAlpha = -Math.PI / 2;
+    let camBeta = Math.PI / 2.5;
+    let camDist = 15;
+    let camTarget = new BABYLON.Vector3(0, 0, 0);
+    let camera = new BABYLON.ArcRotateCamera(
+        "camera1",
+        camAlpha,
+        camBeta,
+        camDist,
+        camTarget,
+        scene
+    );
+    //camera.attachControl(true); // YOU CANNOT ATTACHCONTROL TO CAMERA WITH MULTIPLE SCENES
+    return camera;
 }
 
 function createLight(scene) {
-  //Add a light(s) to the Scene
-  const light = new BABYLON.HemisphericLight(
-    "light",
-    new BABYLON.Vector3(-1, 1, 0),
-    scene
-  );
+    //Add a light(s) to the Scene
+    const light = new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(-1, 1, 0),
+        scene
+    );
 
-  light.intensity = 1;
-  return light;
+    light.intensity = 1;
+    return light;
 }
 
 function createBG(scene) {
-  let hdrTexture = new BABYLON.HDRCubeTexture(
-    "./texture/CosmicCoolCloudBack.hdr",
-    scene,
-    512
-  );
-  let sphere = BABYLON.Mesh.CreateSphere("sphere1", 32, 4, scene);
-  let sphereMtl = new BABYLON.PBRMaterial("sphereMtl", scene);
-  sphereMtl.reflectionTexture = hdrTexture;
-  sphereMtl.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
-  sphereMtl.disableLighting = true;
-  sphereMtl.backFaceCulling = false;
-  sphere.material = sphereMtl;
+    let hdrTexture = new BABYLON.HDRCubeTexture(
+        "./texture/CosmicCoolCloudBack.hdr",
+        scene,
+        512
+    );
+    let sphere = BABYLON.Mesh.CreateSphere("sphere1", 32, 4, scene);
+    let sphereMtl = new BABYLON.PBRMaterial("sphereMtl", scene);
+    sphereMtl.reflectionTexture = hdrTexture;
+    sphereMtl.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    sphereMtl.disableLighting = true;
+    sphereMtl.backFaceCulling = false;
+    sphere.material = sphereMtl;
 
-  sphere.scaling = new BABYLON.Vector3(10, 10, 10);
+    sphere.scaling = new BABYLON.Vector3(10, 10, 10);
 
-  scene.registerBeforeRender(function () {
-    sphere.rotation.z += 0.0001 * scene.deltaTime;
-  });
+    scene.registerBeforeRender(function() {
+        sphere.rotation.z += 0.0001 * scene.deltaTime;
+    });
 
-  return sphere;
+    return sphere;
 }
 
 function createGUI(scene) {
-  let advancedTexture =
-    BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+    let advancedTexture =
+        BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
 
-  let titleImg = new BABYLON.GUI.Image("Title", "./texture/Title.png");
-  titleImg.width = 0.4;
-  titleImg.height = 0.4;
-  titleImg.top = -290;
-  advancedTexture.addControl(titleImg);
+    let titleImg = new BABYLON.GUI.Image("Title", "./texture/Title.png");
+    titleImg.width = 0.4;
+    titleImg.height = 0.4;
+    titleImg.top = -290;
+    advancedTexture.addControl(titleImg);
 
-  let creditImg = new BABYLON.GUI.Image(
-    "Credit",
-    "./texture/Credit.png"
-  );
-  creditImg.width = 0.2;
-  creditImg.height = 0.1;
-  creditImg.top = 375;
-  creditImg.left = 750;
-  advancedTexture.addControl(creditImg);
+    let creditImg = new BABYLON.GUI.Image(
+        "Credit",
+        "./texture/Credit.png"
+    );
+    creditImg.width = 0.2;
+    creditImg.height = 0.1;
+    creditImg.top = 375;
+    creditImg.left = 750;
+    advancedTexture.addControl(creditImg);
 
-  let beginImg = new BABYLON.GUI.Image(
-    "Begin",
-    "./texture/Begin.png"
-  );
-  beginImg.width = 0.4;
-  beginImg.height = 0.125;
-  beginImg.top = 0;
+    let beginImg = new BABYLON.GUI.Image(
+        "Begin",
+        "./texture/Begin.png"
+    );
+    beginImg.width = 0.4;
+    beginImg.height = 0.125;
+    beginImg.top = 0;
 
-  beginImg.onPointerUpObservable.add(function () {
-    setSceneIndex(1);
-  });
-  advancedTexture.addControl(beginImg);
+    beginImg.onPointerUpObservable.add(function() {
+        setSceneIndex(1);
+    });
+    advancedTexture.addControl(beginImg);
 
-  let settingsImg = new BABYLON.GUI.Image(
-    "Settings",
-    "./texture/Settings.png"
-  );
-  settingsImg.width = 0.4;
-  settingsImg.height = 0.125;
-  settingsImg.top = 150;
+    let settingsImg = new BABYLON.GUI.Image(
+        "Settings",
+        "./texture/Settings.png"
+    );
+    settingsImg.width = 0.4;
+    settingsImg.height = 0.125;
+    settingsImg.top = 150;
 
-  settingsImg.onPointerUpObservable.add(function () {
-    alert("I am totally a settings screen");
-  });
-  advancedTexture.addControl(settingsImg);
+    settingsImg.onPointerUpObservable.add(function() {
+        alert("I am totally a settings screen");
+    });
+    advancedTexture.addControl(settingsImg);
 
 }
 
-function createMusic(scene){
-  var music = new BABYLON.Sound("Music", "./audio/e s c p - - Cyber Crime Story.mp3", scene, null, {
-    loop: true,
-    autoplay: true
-  });
+function createMusic(scene) {
+    var music = new BABYLON.Sound("Music", "./audio/e s c p - - Cyber Crime Story.mp3", scene, null, {
+        loop: true,
+        autoplay: true
+    });
 
-  music.setVolume(.1);
-  return music
+    music.setVolume(.1);
+    return music
 }
 
 export default function createStartScene(engine) {
-  let that = {};
-  let scene = (that.scene = new BABYLON.Scene(engine));
-  //scene.debugLayer.show();
+    let that = {};
+    let scene = (that.scene = new BABYLON.Scene(engine));
+    //scene.debugLayer.show();
 
-  /********************************************************************
-   * ANY CODE IN RELATION RENDERING ON THE SCREEN SHOULD GO BELOW HERE
-   ********************************************************************/
+    /********************************************************************
+     * ANY CODE IN RELATION RENDERING ON THE SCREEN SHOULD GO BELOW HERE
+     ********************************************************************/
 
-  scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
-  scene.collisionsEnabled = true;
+    scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
+    scene.collisionsEnabled = true;
 
-  // Enable Collisions
-  scene.collisionsEnabled = true;
+    // Enable Collisions
+    scene.collisionsEnabled = true;
 
-  let light = (that.light = createLight(scene));
-  let camera = (that.camera = createCamera(scene));
+    let light = (that.light = createLight(scene));
+    let camera = (that.camera = createCamera(scene));
 
 
-  let bgSphere = (that.bgSphere = createBG(scene));
+    let bgSphere = (that.bgSphere = createBG(scene));
 
-  let gui = (that.gui = createGUI(scene));
-  let music = (that.music = createMusic(scene));
-  return that;
+    let gui = (that.gui = createGUI(scene));
+    let music = (that.music = createMusic(scene));
+    return that;
 }
--- root/assets/js/game.js
+++ root/assets/js/game.js
@@ -1,20 +1,19 @@
-var canvas = document.getElementById("renderCanvas")
-var engine = new BABYLON.Engine(canvas, true);
-const createScene = function() {
-    const scene = new BABYLON.Scene(engine);
+import { canvas } from "./createScenes.js";
 
-    var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, 10, 0), scene);
+function createCamera(scene) {
+    var camera = new BABYLON.FreeCamera(
+        "FreeCamera",
+        new BABYLON.Vector3(0, 10, 0),
+        scene
+    );
     camera.fov = 0.6;
     camera.inertia = 0.0;
     camera.attachControl(canvas, true);
-
-    scene.enablePhysics(null, new BABYLON.CannonJSPlugin());
 
     //Set the ellipsoid around the camera (e.g. your player's size)
     camera.ellipsoid = new BABYLON.Vector3(1, 4, 1);
 
     camera.applyGravity = true;
-
     camera.enablePhysics = true;
     camera.checkCollisions = true;
 
@@ -25,47 +24,86 @@ const createScene = function() {
     camera.keysRight.push(68);
     camera.keysLeft.push(65);
 
+    return camera;
+}
 
-    const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(1, 1, 0));
+function createLight(scene) {
+    const light = new BABYLON.HemisphericLight(
+        "light",
+        new BABYLON.Vector3(1, 1, 0)
+    );
 
+    return light;
+}
+
+function createActionManager(scene) {
     // Keyboard events
     var inputMap = {};
     scene.actionManager = new BABYLON.ActionManager(scene);
-    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function(evt) {
-        inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
-    }));
-    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, function(evt) {
-        inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
-    }));
+    scene.actionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnKeyDownTrigger,
+            function(evt) {
+                inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+            }
+        )
+    );
+    scene.actionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(
+            BABYLON.ActionManager.OnKeyUpTrigger,
+            function(evt) {
+                inputMap[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+            }
+        )
+    );
+}
 
-    BABYLON.SceneLoader.Append("../../../root/assets/scenes/", "scene.babylon", scene, (newMeshes) => {
-        var meshes = newMeshes.meshes;
-        meshes.forEach(item => {
-            item.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
-        });
-    });
+function appendScene(scene) {
+    BABYLON.SceneLoader.Append(
+        "../../../BJS Editor/assets/EngineRoom/",
+        "scene.babylon",
+        scene,
+        (newMeshes) => {
+            var meshes = newMeshes.meshes;
+            meshes.forEach((item) => {
+                item.physicsImpostor = new BABYLON.PhysicsImpostor(
+                    ground,
+                    BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 },
+                    scene
+                );
+            });
+        }
+    );
+}
 
+function createControls(scene, camera) {
     //Controls...Mouse
     //We start without being locked.
     var isLocked = false;
 
     // On click event, request pointer lock
     scene.onPointerDown = function(evt) {
-
         //true/false check if we're locked, faster than checking pointerlock on each single click.
         if (!isLocked) {
-            canvas.requestPointerLock = canvas.requestPointerLock || canvas.msRequestPointerLock || canvas.mozRequestPointerLock || canvas.webkitRequestPointerLock;
+            canvas.requestPointerLock =
+                canvas.requestPointerLock ||
+                canvas.msRequestPointerLock ||
+                canvas.mozRequestPointerLock ||
+                canvas.webkitRequestPointerLock;
             if (canvas.requestPointerLock) {
                 canvas.requestPointerLock();
             }
         }
-
     };
-
 
     // Event listener when the pointerlock is updated (or removed by pressing ESC for example).
     var pointerlockchange = function() {
-        var controlEnabled = document.mozPointerLockElement || document.webkitPointerLockElement || document.msPointerLockElement || document.pointerLockElement || null;
+        var controlEnabled =
+            document.mozPointerLockElement ||
+            document.webkitPointerLockElement ||
+            document.msPointerLockElement ||
+            document.pointerLockElement ||
+            null;
 
         // If the user is already locked
         if (!controlEnabled) {
@@ -96,27 +134,51 @@ const createScene = function() {
                 break;
         }
     });
-    var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI('Ui');
-    var crosschair = BABYLON.GUI.Button.CreateImageOnlyButton("b1", "../../../BJS Editor/scenes/EngineRoom/textures/pngfind.com-crosshair-dot-png-5191877.png")
+    var advancedTexture =
+        BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("Ui");
+    var crosschair = BABYLON.GUI.Button.CreateImageOnlyButton(
+        "b1",
+        "../../../BJS Editor/scenes/EngineRoom/textures/pngfind.com-crosshair-dot-png-5191877.png"
+    );
     crosschair.image.stretch = BABYLON.GUI.Image.STRETCH_UNIFORM;
     crosschair.width = "48px";
     crosschair.height = "48px";
-    crosschair.color = "transparent"
+    crosschair.color = "transparent";
     advancedTexture.addControl(crosschair);
 
     // Attach events to the document
     document.addEventListener("pointerlockchange", pointerlockchange, false);
     document.addEventListener("mspointerlockchange", pointerlockchange, false);
     document.addEventListener("mozpointerlockchange", pointerlockchange, false);
-    document.addEventListener("webkitpointerlockchange", pointerlockchange, false);
+    document.addEventListener(
+        "webkitpointerlockchange",
+        pointerlockchange,
+        false
+    );
+}
 
-    scene.debugLayer.show();
-    return scene;
-};
+export default function createStartScene(engine) {
+    let that = {};
+    let scene = (that.scene = new BABYLON.Scene(engine));
+    //scene.debugLayer.show();
 
+    /********************************************************************
+     * ANY CODE IN RELATION RENDERING ON THE SCREEN SHOULD GO BELOW HERE
+     ********************************************************************/
 
-var scene = createScene();
-engine.runRenderLoop(function() {
+    scene.enablePhysics(null, new BABYLON.CannonJSPlugin());
+    scene.collisionsEnabled = true;
 
-    scene.render();
-});
+    let light = (that.light = createLight(scene));
+    let camera = (that.camera = createCamera(scene));
+    let manager = (that.actionManager = createActionManager(scene));
+    let append = (that.append = appendScene(scene));
+
+    let controls = (that.controls = createControls(scene, camera));
+    //let gui = (that.gui = createGUI(scene));
+
+    //let bgSphere = (that.bgSphere = createBG(scene));
+
+    //let music = (that.music = createMusic(scene));
+    return that;
+}
