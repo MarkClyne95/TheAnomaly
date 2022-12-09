0af4f8a1acd076626cc00457f958468537bc8903
MarkClyne95 <99036942+MarkClyne95@users.noreply.github.com>
2022-12-09 16:21:47 +0000

    Update PlayVideo.js

--- root/assets/js/new js/PlayVideo.js
+++ root/assets/js/new js/PlayVideo.js
@@ -2,6 +2,7 @@ import { canvas, setSceneIndex, sceneIndex } from "../createScenes.js";
 
 let scene;
 let engine;
+let played = false;
 
 export default class PlayVideo {
     constructor(engine, scene) {
@@ -17,56 +18,60 @@ export default class PlayVideo {
         // This creates and positions a free camera (non-mesh)
         var camera = new BABYLON.ArcRotateCamera("arcR", -Math.PI / 2, Math.PI / 2, 800, BABYLON.Vector3.Zero(), scene);
 
+
+
+        scene.registerAfterRender(() => {
+            if (!played) {
+                played = true;
+                this.PVideo(scene, engine, videoUrl, video2);
+            }
+        })
+
+        //console.log(ANote0Video);
+        return scene;
+    };
+
+    PVideo(scene, engine, v1, v2) {
         var planeOpts = {
             height: 1080,
             width: 1920,
             sideOrientation: BABYLON.Mesh.DOUBLESIDE
         };
+        var ANote0Video = BABYLON.MeshBuilder.CreatePlane("plane", planeOpts, scene);
+        var vidPos = (new BABYLON.Vector3(0, 0, 0.1))
+        ANote0Video.position = vidPos;
+        var ANote0VideoMat = new BABYLON.StandardMaterial("m", scene);
+        var ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex", v1, scene);
+        ANote0VideoMat.diffuseTexture = ANote0VideoVidTex;
+        ANote0VideoMat.roughness = 1;
+        ANote0VideoMat.emissiveColor = new BABYLON.Color3.White();
+        ANote0Video.material = ANote0VideoMat;
+        ANote0VideoVidTex.video.loop = false;
 
-        if (scene.isReady()) {
-            scene.onPointerDown = function() {
-                var ANote0Video = BABYLON.MeshBuilder.CreatePlane("plane", planeOpts, scene);
-                var vidPos = (new BABYLON.Vector3(0, 0, 0.1))
-                ANote0Video.position = vidPos;
-                var ANote0VideoMat = new BABYLON.StandardMaterial("m", scene);
-                var ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex", videoUrl, scene);
-                ANote0VideoMat.diffuseTexture = ANote0VideoVidTex;
-                ANote0VideoMat.roughness = 1;
-                ANote0VideoMat.emissiveColor = new BABYLON.Color3.White();
-                ANote0Video.material = ANote0VideoMat;
-                ANote0VideoVidTex.video.loop = false;
-
-                ANote0VideoVidTex.video.addEventListener('play', (event) => {
-                    switch (sceneIndex) {
-                        case 1:
-                            ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex", videoUrl, scene);
-                            break;
-                        case 4:
-                            ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex", video2, scene);
-                            break;
-                    }
-                })
+        ANote0VideoVidTex.video.addEventListener('play', (event) => {
+            switch (sceneIndex) {
+                case 1:
+                    ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex", v1, scene);
+                    break;
+                case 4:
+                    ANote0VideoVidTex = new BABYLON.VideoTexture("vidtex", v2, scene);
+                    break;
+            }
+        })
 
 
-                ANote0VideoVidTex.video.addEventListener('ended', (event) => {
-                    switch (sceneIndex) {
-                        case 1:
-                            setSceneIndex(2)
-                            scene.onPointerDown = null;
-                            break;
-                        case 4:
-                            location.reload();
-                            scene.onPointerDown = null;
-                            break;
-                    }
-                })
-            };
-        }
-
-
-
-        scene.on
-            //console.log(ANote0Video);
-        return scene;
-    };
+        ANote0VideoVidTex.video.addEventListener('ended', (event) => {
+            switch (sceneIndex) {
+                case 1:
+                    ANote0VideoVidTex.dispose();
+                    setSceneIndex(2)
+                    scene.onPointerDown = null;
+                    break;
+                case 4:
+                    location.reload();
+                    scene.onPointerDown = null;
+                    break;
+            }
+        })
+    }
 }
